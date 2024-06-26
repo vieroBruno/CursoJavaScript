25145e450d9c91dbf53bbc67e070c8358082cc61
Bruno <viero.g.bruno@gmail.com>
2024-06-26 18:53:21 -0300

    Aulas 26-06

---
+++ JavaScript-2/objetos/objetos.js
@@ -0,0 +1,25 @@
+let nome = "prop"
+console.log(nome)
+let objeto = {}
+objeto.prop = "Olá mundo"
+console.log(objeto)
+objeto[nome]
+
+let funcao = "log"
+console.log(funcao)
+console[funcao]("Olá mundo")
+const pessoa = {}
+pessoa.nome = "Bruno"
+console.log(pessoa)
+pessoa.idade = 19
+console.log(pessoa)
+
+pessoa.colegas = ["Pele","Lucas","Rafael"]
+console.log(pessoa)
+pessoa.endereco={
+    rua: "aqui",
+    numero:42,
+    bairro:"centro"
+}
+
+console.log(pessoa)
