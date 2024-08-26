9e3638c70a192849665919eda596a5a8d4780b84
Bruno <viero.g.bruno@gmail.com>
2024-08-26 18:39:04 -0300

    Funções de listar,visualizar,excluir e criar prontas -26-08

--- JavaScript-2/Exercicio10/index.js
+++ JavaScript-2/Exercicio10/index.js
@@ -1,25 +1,89 @@
 let escolha = "1"
+let vagas = {
+    candidato: []
+}
 do {
     escolha = mostrarMenu()
     switch (escolha) {
             case "1":
-                alert("Opção para listar vagas disponiveis escolhida!")
+                if(vagas.length  > 0){
+                    for (let i = 0; i < vagas.length; i++) {
+                            alert(
+                                "Vaga:" +(i+1)+
+                                "\nNome: " + vagas[i].nome +
+                                "\nDescrição: " + vagas[i].descricao +
+                                "\nData Limite: " + vagas[i].dataLimite 
+                            )
+                        
+                    }
+                }else{
+                    alert("Não há vagas disponiveis!")
+                }
             break;
 
             case "2":
-                alert("Opção para criar vagas escolhida!")
-            break;
 
+                alert("Opção para criar vagas escolhida!")
+                const vaga = {}
+                vaga.nome=prompt("Nome da vaga: ")
+                vaga.descricao=prompt("Descrição da Vaga: ")
+                vaga.dataLimite=prompt("Data Limite: ")
+
+                const confirmacao = confirm(
+                    "Salvar esta vaga?\n"+
+                    "\nNome: " + vaga.nome +
+                    "\nDescrição: " + vaga.descricao +
+                    "\nDataLimite: " + vaga.dataLimite 
+                )
+
+                if(confirmacao){
+                    vagas.push(vaga)
+                } 
+                alert("Vaga salva com sucesso")
+            break;
             case "3":
-                alert("Opção para visualizar vagas escolhida!")
+                alert("Opção para visualizar uma vaga escolhida!")
+                let j = parseFloat(prompt("Qual o indice da vaga?"))
+                alert(
+                            "Vaga" +(j+1)+
+                            "\nNome: " + vagas[j].nome +
+                            "\nDescrição: " + vagas[j].descricao +
+                            "\nData Limite: " + vagas[j].dataLimite +
+                            "\nQuantidade de Candidatos: " + vagas[j].qtdCandidato +
+                            "\nNomes Candidatos: " + vagas[j].candidatos)
             break;
 
             case "4":
                 alert("Opção para inscrever um candidato em uma  vaga escolhida!")
+                const candidato = prompt("Qual o nome do candidato?")
+                let k = parseFloat(prompt("Qual o indice da vaga?"))
+                alert(
+                    "Vaga" +(k+1)+
+                    "\nNome: " + vagas[k].nome +
+                    "\nDescrição: " + vagas[k].descricao +
+                    "\nData Limite: " + vagas[k].dataLimite)
+                vagas[k].candidatos.push(candidato)
+                vagas[k].qtdCandidato++
+                alert("Candidato inscrito com sucesso!")
             break;
 
             case "5":
                 alert("Opção para excluir vagas escolhida!")
+                let i = parseFloat(prompt("Qual o indice da vaga?"))
+
+                if(i >= 0 && i < vagas.length ){
+                    const excluir = confirm(
+                            "Deseja excluir essa vaga?"+
+                            "Vaga" +(i+1)+
+                            "\nNome: " + vagas[i].nome +
+                            "\nDescrição: " + vagas[i].descricao +
+                            "\nData Limite: " + vagas[i].dataLimite)
+                        if(excluir){
+                            vagas.splice(i,1)
+                        }
+                } else {
+                    alert("Indice Inválido.Tente Novamente!")
+                }
             break;
 
             case "6":
